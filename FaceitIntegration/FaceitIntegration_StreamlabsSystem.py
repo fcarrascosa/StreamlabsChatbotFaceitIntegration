e7cdb8da8d8c81105b35a4315dd33f16943ebd17
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2021-02-21 21:14:22 +0100

    feat(core): validate settings after saving them

--- FaceitIntegration/FaceitIntegration_StreamlabsSystem.py
+++ FaceitIntegration/FaceitIntegration_StreamlabsSystem.py
@@ -1,7 +1,12 @@
+import codecs
 import os
 import sys
+import json
+from collections import namedtuple
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "lib"))
+sys.path.append(os.path.join(os.path.dirname(__file__), "assets"))
+import messaging
 from file_system import create_directory
 from script_settings import FaceitIntegrationSettings
 
@@ -21,6 +26,8 @@ Version = "0.3.1"
 SETTINGS_DIRECTORY = os.path.join(os.path.dirname(__file__), 'settings')
 SETTINGS_FILE = os.path.join(SETTINGS_DIRECTORY, 'settings.json')
 SETTINGS = FaceitIntegrationSettings()
+ASSETS_DIRECTORY = os.path.join(os.path.dirname(__file__), 'assets')
+MESSAGES_DIRECTORY = os.path.join(ASSETS_DIRECTORY, 'messages')
 
 
 # ---------------------------
@@ -34,6 +41,7 @@ def Init():
 
     create_directory(SETTINGS_DIRECTORY)
     SETTINGS.save(SETTINGS_FILE, ScriptName, Parent)
+    validate_settings()
     return
 
 
@@ -62,6 +70,25 @@ def ReloadSettings(json_data):
     """
 
     SETTINGS.load_settings(json_data)
-    Parent.Log("SYSTEM", "RELOAD SETTINGS")
     SETTINGS.save(SETTINGS_FILE, ScriptName, Parent)
+    validate_settings()
     return
+
+
+# ---------------------------
+#   Validation Functions
+# ---------------------------
+def validate_settings():
+    if not SETTINGS.validate_core_fields():
+        messaging.show_dialog(get_message('config', 'error_config_header'), get_message('config', 'error_config_body'))
+    return
+
+
+# ---------------------------
+#   Messaging Functions
+# ---------------------------
+def get_message(message_type='', key=''):
+    message_file = os.path.join(MESSAGES_DIRECTORY, message_type + '.json')
+    with codecs.open(message_file, encoding='utf-8-sig', mode='r') as f:
+        obj = json.load(f, encoding='utf-8')
+    return obj[key]
