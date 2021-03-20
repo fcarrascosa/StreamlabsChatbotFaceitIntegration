d5648d02842847157b689cfc94fd2c1697d47969
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2021-03-20 07:02:12 +0100

    feat(api-client): add getter for match history

--- FaceitIntegration/lib/faceit_api_client.py
+++ FaceitIntegration/lib/faceit_api_client.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 
 class FaceitApiClient:
@@ -21,3 +22,15 @@ class FaceitApiClient:
             "elo": player_info["games"]["csgo"]["faceit_elo"],
             "level": player_info["games"]["csgo"]["skill_level"]
         }
+
+    def get_match_history(self, nick_name, get_from=None):
+        player_info = self.get_player(nick_name)
+        player_id = player_info["player_id"]
+        request_url = self.api_base_url + "players/" + player_id + "/history?game=csgo"
+        request_url = request_url + "&from=" + str(get_from) if get_from else request_url
+        request_url = request_url + "&to=" + str(int((datetime.datetime.now() - datetime.datetime(1970,1,1)).total_seconds()))
+        request_url = request_url + "&limit=" + "100"
+        request_headers = {"Authorization": "Bearer " + self.api_key}
+        result = self.parent.GetRequest(request_url, request_headers)
+        parsed_result = json.loads(result)["response"]
+        return json.loads(parsed_result)["items"]
