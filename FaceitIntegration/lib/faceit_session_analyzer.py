74139da11bd8865ed2b171cb0f19a3f1f94f09fc
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2021-03-20 07:04:38 +0100

    feat(session-analyzer): add class for analysis on a stream session

---
+++ FaceitIntegration/lib/faceit_session_analyzer.py
@@ -0,0 +1,52 @@
+import datetime
+
+
+class FaceitSessionAnalyzer:
+    def __init__(self, nick_name, get_all=False):
+        self.session_init = None
+        self.nick_name = nick_name
+        self.get_all = get_all
+        self.matches = []
+        self.initial_elo = 0
+        self.initial_matches = []
+
+    def start_session(self, initial_elo, matches):
+        self.session_init = int((datetime.datetime.now() - datetime.datetime(1970, 1, 1)).total_seconds())
+        self.initial_elo = initial_elo
+
+        matches_to_init = matches
+        if not self.get_all:
+            matches_to_init = [match for match in matches if match["competition_type"] == "matchmaking"]
+        self.initial_matches = matches_to_init
+
+    def set_matches(self, matches):
+
+        matches = [match for match in matches if match not in self.initial_matches]
+
+        if not self.get_all:
+            matches = [match for match in matches if
+                       match["competition_type"] == "matchmaking"]
+        self.matches = matches
+
+    def is_won_match(self, match=None):
+        player_faction = [key for key, value in match["teams"].items() if
+                          self.nick_name in [player["nickname"] for player in value["players"]]][0]
+        winner_faction = match["results"]["winner"]
+        return True if player_faction == winner_faction else False
+
+    def count_won_matches(self):
+        value = 0
+
+        for match in self.matches:
+            value += 1 if self.is_won_match(match) else 0
+
+        return value
+
+    def count_total_matches(self):
+        return len(self.matches)
+
+    def count_lost_matches(self):
+        return self.count_total_matches() - self.count_won_matches()
+
+    def get_session_elo(self, current_elo):
+        return current_elo - self.initial_elo
