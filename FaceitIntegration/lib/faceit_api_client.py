1886087e3e0d055ecdbbd7f44603a9bc34faf484
Fernando Carrascosa <fcarrascosa@fcarrascosa.es>
2021-03-31 04:08:57 +0200

    fix(session-analyzer): fix problem with EPOCH datetime

--- FaceitIntegration/lib/faceit_api_client.py
+++ FaceitIntegration/lib/faceit_api_client.py
@@ -1,5 +1,6 @@
 import json
 import datetime
+import time
 
 
 class FaceitApiClient:
@@ -23,12 +24,12 @@ class FaceitApiClient:
             "level": player_info["games"]["csgo"]["skill_level"]
         }
 
-    def get_match_history(self, nick_name, get_from=None):
+    def get_match_history(self, nick_name, get_from=None, get_to=None):
         player_info = self.get_player(nick_name)
         player_id = player_info["player_id"]
         request_url = self.api_base_url + "players/" + player_id + "/history?game=csgo"
         request_url = request_url + "&from=" + str(get_from) if get_from else request_url
-        request_url = request_url + "&to=" + str(int((datetime.datetime.now() - datetime.datetime(1970,1,1)).total_seconds()))
+        request_url = request_url + "&to=" + (str(get_to) if get_to else str(int(time.mktime(datetime.datetime.now().timetuple()))))
         request_url = request_url + "&limit=" + "100"
         request_headers = {"Authorization": "Bearer " + self.api_key}
         result = self.parent.GetRequest(request_url, request_headers)
